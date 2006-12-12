eb71a6b246174ac2cdf5de077575795012cccdf1
cmsbuild <>
2006-12-12 22:10:08 +0000

    This commit was manufactured by cvs2git to create tag 'RecoEgamma- EgammaElectronProducers-CMSSW_1_2_0_g4_81'.
    Sprout from master 2006-12-12 22:10:04 UTC David Futyan <sha1-9b33e1ceb82d36509264da3be42fb8b223a33816@cern.ch> 'use correctedEndcapSuperClustersWithPreshower as input'
    Cherrypick from master 2006-11-17 16:16:47 UTC uberthon <sha1-79a1dca24b5010d458ca508db84b2ac655c0b091@cern.ch> 'improve configuration':
        interface/ElectronAnalyzer.h
        src/ElectronAnalyzer.cc
        src/PixelMatchElectronAnalyzer.cc
        src/PixelMatchElectronProducer.cc
        src/SealModule.cc
        src/TrackProducerWithSeedAssoc.cc
    Delete:
        data/egammaGsfCtfWithMaterialTracks.cff
        data/egammaGsfCtfWithMaterialTracks.cfi
        data/electronAnalyzer.cfi
        data/pixelMatchGsfElectronSequence.cff
        data/pixelMatchGsfElectrons.cff
        data/pixelMatchGsfElectrons.cfi
        interface/GsfTrackProducerWithSeedAssoc.h
        interface/PixelMatchGsfElectronAnalyzer.h
        interface/PixelMatchGsfElectronProducer.h
        src/GsfTrackProducerWithSeedAssoc.cc
        src/PixelMatchGsfElectronAnalyzer.cc
        src/PixelMatchGsfElectronProducer.cc
        test/egammaRecHitsToPixelMatchGsfElectrons.cfg
        test/read_gsfElectrons.cfg
        test/runElectronAnalyzer.cfg

--- src/PixelMatchElectronProducer.cc
+++ src/PixelMatchElectronProducer.cc
@@ -1,11 +1,11 @@
 // -*- C++ -*-
 //
 // Package:    EgammaElectronProducers
-// Class:      PixelMatchGsfElectronProducer
+// Class:      PixelMatchElectronProducer
 // 
-/**\class PixelMatchGsfElectronProducer RecoEgamma/ElectronProducers/src/PixelMatchGsfElectronProducer.cc
+/**\class PixelMatchElectronProducer RecoEgamma/ElectronProducers/src/PixelMatchElectronProducer.cc
 
- Description: EDProducer of PixelMatchGsfElectron objects
+ Description: EDProducer of Electron objects
 
  Implementation:
      <Notes on implementation>
@@ -13,20 +13,26 @@
 //
 // Original Author:  Ursula Berthon, Claude Charlot
 //         Created:  Mon Mar 27 13:22:06 CEST 2006
-// $Id: PixelMatchGsfElectronProducer.cc,v 1.9 2006/10/27 16:34:21 uberthon Exp $
+// $Id: PixelMatchElectronProducer.cc,v 1.8 2006/10/27 15:04:05 uberthon Exp $
 //
 //
 
 // user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+#include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
 
-#include "RecoEgamma/EgammaElectronProducers/interface/PixelMatchGsfElectronProducer.h"
+#include "RecoEgamma/EgammaElectronProducers/interface/PixelMatchElectronProducer.h"
 #include "RecoEgamma/EgammaElectronAlgos/interface/PixelMatchElectronAlgo.h"
+#include "DataFormats/EgammaReco/interface/ElectronPixelSeedFwd.h"
 #include "DataFormats/EgammaReco/interface/ElectronPixelSeed.h"
 #include "DataFormats/EgammaReco/interface/SeedSuperClusterAssociation.h"
-#include "DataFormats/TrackReco/interface/GsfTrack.h"
+#include "DataFormats/TrackReco/interface/TrackFwd.h"
+#include "DataFormats/TrackReco/interface/TrackExtraFwd.h"
+#include "DataFormats/TrackReco/interface/Track.h"
 #include "DataFormats/TrackCandidate/interface/TrackCandidateCollection.h"
 #include "DataFormats/TrackingRecHit/interface/TrackingRecHitFwd.h"
 #include "DataFormats/EgammaCandidates/interface/PixelMatchGsfElectronFwd.h"
@@ -35,7 +41,7 @@
 
 using namespace reco;
  
-PixelMatchGsfElectronProducer::PixelMatchGsfElectronProducer(const edm::ParameterSet& iConfig) : conf_(iConfig)
+PixelMatchElectronProducer::PixelMatchElectronProducer(const edm::ParameterSet& iConfig) : conf_(iConfig)
 {
   //register your products
   produces<PixelMatchGsfElectronCollection>();
@@ -52,18 +58,18 @@ PixelMatchGsfElectronProducer::PixelMatchGsfElectronProducer(const edm::Paramete
 }
 
 
-PixelMatchGsfElectronProducer::~PixelMatchGsfElectronProducer()
+PixelMatchElectronProducer::~PixelMatchElectronProducer()
 {
   delete algo_;
 }
 
-void PixelMatchGsfElectronProducer::beginJob(edm::EventSetup const&iSetup) 
+void PixelMatchElectronProducer::beginJob(edm::EventSetup const&iSetup) 
 {     
   algo_->setupES(iSetup,conf_);  
 }
 
 // ------------ method called to produce the data  ------------
-void PixelMatchGsfElectronProducer::produce(edm::Event& e, const edm::EventSetup& iSetup) 
+void PixelMatchElectronProducer::produce(edm::Event& e, const edm::EventSetup& iSetup) 
 {
 
   // Create the output collections   
